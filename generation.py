6875c006bcbd273f3534bb3c91d09de1d0fc6b9c
tantoni228 <119693559+tantoni228@users.noreply.github.com>
2023-09-01 19:16:26 +0300

    Update generation.py
    English version

--- generation.py
+++ generation.py
@@ -3,12 +3,12 @@ from copy import deepcopy
 
 class Generation_sudoku:
     def __init__(self, level):
-        self.level = level  # Задаётся уровень сложности
-        self.template = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # Шаблон строки
-        self.sudoku = []  # Данные судоку (шаблон)
+        self.level = level  # The difficulty level is set
+        self.template = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # String Template
+        self.sudoku = []  # Sudoku data (template)
         self.sudoku2 = []  # Судоку с пробелами
         self.sudoku3 = []
-        for i in range(3):  # Генерация шаблона судоку
+        for i in range(3):  # Sudoku with spaces
             self.line = self.template.copy()
             for j in range(3):
                 self.sudoku.append([])
@@ -30,30 +30,30 @@ class Generation_sudoku:
                     self.sudoku[i * 3 + j].append(self.line)
 
     def transport_1_rows(self):
-        self.y_big = randint(0, 2)  # Место в большой строке где будет происходиться перемешивание
-        self.y_small1, self.y_small2 = sample([0, 1, 2], 2)  # Место маленьких строк, которые будут перемешиваться между собой
+        self.y_big = randint(0, 2)  # The place in the big row where the mixing will take place
+        self.y_small1, self.y_small2 = sample([0, 1, 2], 2)  # Place small strings that will be shuffled together
         self.sudoku[self.y_big * 3 + self.y_small1], self.sudoku[self.y_big * 3 + self.y_small2] = self.sudoku[self.y_big * 3 + self.y_small2], self.sudoku[self.y_big * 3 + self.y_small1]
 
     def transport_1_columns(self):
-        self.x_big = randint(0, 2)  # Место в большом столбце где будет происходиться перемешивание
-        self.x_small1, self.x_small2 = sample([0, 1, 2], 2)  # Место в маленьких столбцах которые будут перемешиваться между собой
-        for i in range(9):  # Перестановка каждого элемета в столбцы
+        self.x_big = randint(0, 2)  # The place in the large column where the mixing will take place
+        self.x_small1, self.x_small2 = sample([0, 1, 2], 2)  # Place in small columns that will mix with each other
+        for i in range(9):  # Rearranging each element into columns
             self.sudoku[i][0][self.x_big * 3 + self.x_small1], self.sudoku[i][0][self.x_big * 3 + self.x_small2] = self.sudoku[i][0][self.x_big * 3 + self.x_small2], self.sudoku[i][0][self.x_big * 3 + self.x_small1]
 
     def transport_3_rows(self):
-        self.y_big1, self.y_big2 = sample([0, 1, 2], 2)  # Большие строки, которые будут перемешиваться между собой
-        for h in range(3):  # Перестановка каждой строки
+        self.y_big1, self.y_big2 = sample([0, 1, 2], 2)  # Large strings that will be shuffled together
+        for h in range(3):  # Rearranging each row
             self.sudoku[self.y_big1 * 3 + h], self.sudoku[self.y_big2 * 3 + h] = self.sudoku[self.y_big2 * 3 + h], self.sudoku[self.y_big1 * 3 + h]
 
     def transport_3_columns(self):
-        self.x_big1, self.x_big2 = sample([0, 1, 2], 2)  # Большие столбцы, которые будут перемешиваться между собой
-        for i in range(3):  # Перестановка каждого столбца
-            for j in range(9):  # Перестановка каждого элемета в столбцы
+        self.x_big1, self.x_big2 = sample([0, 1, 2], 2)  # Large columns that will be shuffled together
+        for i in range(3):  # Rearranging each column
+            for j in range(9):  # Rearranging each element into columns
                 self.sudoku[j][0][self.x_big1 * 3 + i], self.sudoku[j][0][self.x_big2 * 3 + i] = self.sudoku[j][0][self.x_big2 * 3 + i], self.sudoku[j][0][self.x_big1 * 3 + i]
 
-    def mix(self):  # Перемешивание судоку
+    def mix(self):  # Shuffling Sudoku
         self.n = randint(30, 45)
-        for i in range(self.n):  # Примение каждого приёма перемешивания
+        for i in range(self.n):  # Application of each mixing technique
             self.transport_1_rows()
             self.transport_3_columns()
             self.transport_3_rows()
@@ -65,22 +65,21 @@ class Generation_sudoku:
 
     def eating_cells(self):
         if self.level == "лёгкий":
-            self.n = randint(31, 35)  # Количестов подсказок на лёгком уровне
+            self.n = randint(31, 35)  # Number of hints at an easy level
         elif self.level == "средний":
-            self.n = randint(26, 30)  # Количестов подсказок на среднем уровне
+            self.n = randint(26, 30)  # The number of hints at the average level
         elif self.level == "сложный":
-            self.n = randint(23, 25)  # Количестов подсказок на сложном уровне
-        self.long = 81  # Количесто неудаленных клеток
+            self.n = randint(23, 25)  # Number of hints at a difficult level
+        self.long = 81  # The number of undeleted cells
         while self.long != self.n:
             self.x = randint(0, 8)
             self.y = randint(0, 8)
-            if self.sudoku2[self.y][self.x] == 0:  # Условие, для того чтобы не повторять одну операцию несколько раз
+            if self.sudoku2[self.y][self.x] == 0:  # Condition, in order not to repeat the same operation several times
                 continue
             self.sudoku2[self.y][self.x] = 0
-            if solve(self.sudoku2) == self.sudoku3:  # Проверка на одно решение при "поедание одной клетки"
+            if solve(self.sudoku2) == self.sudoku3:  # Checking for one solution when "eating one cell"
                 self.long -= 1
             else:
-                self.sudoku2[self.y][self.x] = self.sudoku3[self.y][self.x]  # Возращаем значение, если не возможно
-
-    def listing(self):  # Возвращает данные сгенерированного судоку
+                self.sudoku2[self.y][self.x] = self.sudoku3[self.y][self.x]  # Return the value if not possible
+    def listing(self):  # Returns the data of the generated Sudoku
         return self.sudoku3, self.sudoku2
